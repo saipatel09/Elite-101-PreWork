e312b5d155696feb1403a04320b117e46e466ae9
saipatel0916 <26700175-saipatel0916@users.noreply.replit.com>
2024-01-09 00:58:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Welcome to my Chatbox! \n')
+
+name = input('May I ask what your name is? \n')
+age = input('And also tell me your age please.\n')
+help = input('How may I help you today? \n')
+
+print('Okay. Choose from the options below to continue. \n')
+options =  input('1, 2, 3, 4. \n')
+if options == '4':
+  print("Thank you for using the program!")
+else:
+  print("Sorry, I don't understand.")
